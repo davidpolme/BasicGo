cc3cce7158b76b433fc6eda2897b2a2c5fcdc094
davidpolme <david.polania3@gmail.com>
2021-09-16 13:57:50 -0500

    Structs & Interfaces en un paquete aparte

--- src/main.go
+++ src/main.go
@@ -1,42 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type figures2D interface {
-	area() float64
-}
-
-type square struct {
-	base float64
-}
-
-type rectangle struct {
-	base   float64
-	height float64
-}
-
-func (c square) area() float64 {
-	return c.base * c.base
-}
-
-func (r rectangle) area() float64 {
-	return r.base * r.height
-}
-
-func calcularArea(f figures2D) {
-	fmt.Println("Area: ", f.area())
-}
+	fn "github.com/davidpolme/BasicGo/src/functions"
+)
 
 func main() {
-	mySquare := square{base: 2}
-	myRectangle := rectangle{base: 2, height: 4}
+	mySquare := fn.Square{Base: 2}
+	myRectangle := fn.Rectangle{Base: 2, Height: 4}
 
-	calcularArea(myRectangle)
-	calcularArea(mySquare)
+	fn.CalcularArea(myRectangle)
+	fn.CalcularArea(mySquare)
 
 	//Lista de interfaces
-
 	myInterface := []interface{}{"Hola", 12, 4.90}
-
 	fmt.Println(myInterface...)
 }

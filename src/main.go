90558b441af4a9b1b79788b4eb97953c2dc13430
davidpolme <david.polania3@gmail.com>
2021-09-16 03:37:48 -0500

    Structs y punteros

--- src/main.go
+++ src/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/davidpolme/BasicGo/src/mypkg"
+)
 
 func main() {
 
@@ -8,6 +12,7 @@ func main() {
 		& Sirve para acceder a la direccion de memoria
 		* Sirve para acceder al valor de memoria
 	*/
+
 	a := 50
 	b := &a
 
@@ -16,4 +21,23 @@ func main() {
 	//a y *b apuntan a la misma direccion de memoria
 	*b = 100
 	fmt.Println("\na : ", a, "\nb : ", b, "\nb*: ", *b)
+
+	myPC := mypkg.Pc{Ram: 16, Disk: 200, Brand: "msi"}
+
+	//fmt.Println("\nMi pc:", "\n- Ram:", myPc.ram, "\n- Disk:", myPc.disk, "\n- Brand:", myPc.brand)
+	fmt.Printf(`Mi pc: 
+	- Ram:   %d 
+	- Disk:  %d 
+	- Brand: %s
+`, myPC.Ram, myPC.Disk, myPC.Brand)
+
+	myPC.Ping()
+
+	fmt.Println(myPC)
+	myPC.DuplicateRAM()
+
+	fmt.Println(myPC)
+	myPC.DuplicateRAM()
+
+	fmt.Println(myPC)
 }

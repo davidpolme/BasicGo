f1307cdca090b83fefd9cc8a121c50cd958da967
davidpolme <david.polania3@gmail.com>
2021-09-16 13:40:37 -0500

    Interfaces y listas de interfaces

--- src/main.go
+++ src/main.go
@@ -1,12 +1,42 @@
 package main
 
-import (
-	"fmt"
+import "fmt"
 
-	"github.com/davidpolme/BasicGo/src/mypkg"
-)
+type figures2D interface {
+	area() float64
+}
+
+type square struct {
+	base float64
+}
+
+type rectangle struct {
+	base   float64
+	height float64
+}
+
+func (c square) area() float64 {
+	return c.base * c.base
+}
+
+func (r rectangle) area() float64 {
+	return r.base * r.height
+}
+
+func calcularArea(f figures2D) {
+	fmt.Println("Area: ", f.area())
+}
 
 func main() {
-	myPC := mypkg.Pc{Ram: 16, Disk: 200, Brand: "msi"}
-	fmt.Println(myPC)
+	mySquare := square{base: 2}
+	myRectangle := rectangle{base: 2, height: 4}
+
+	calcularArea(myRectangle)
+	calcularArea(mySquare)
+
+	//Lista de interfaces
+
+	myInterface := []interface{}{"Hola", 12, 4.90}
+
+	fmt.Println(myInterface...)
 }
